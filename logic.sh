3cd24dbd745d9ed85b63ce357680f6b9c57f9cd8
Martin Gergov <martingergov1@gmail.com>
2016-11-09 17:09:37 +0100

    queries for bash version

--- logic.sh
+++ logic.sh
@@ -5,9 +5,12 @@ mkdir root
 
 #return variable
 return=""
-
 function in_tree {
-    return=$(find "$2" -name "$1")
+    if [ -d $1 ]; then
+    	return="$1"
+    else
+    	return=""
+    fi    
 }
 
 function find_functor {
@@ -23,43 +26,51 @@ function f {
     args=(${@:2})
     find_functor $functor
     func_path=$return
-    in_tree ${args[0]} $return
-    if [ "$return" =  "" ]; then
-	mkdir $func_path/${args[0]}
-	mkdir $func_path/${args[0]}/${args[1]}
-    else
-	node_path=$return
-	in_tree ${args[1]} $return
-	if [ "$return" =  "" ]; then
-	    mkdir $node_path/${args[1]}
-	fi
+    arg_path=$( IFS=$'/'; echo "${args[*]}" )
+    in_tree "$func_path/$arg_path"
+    if [ "$return" = "" ]; then
+	mkdir -p "$func_path/$arg_path"
     fi
 }
 
 function q {
-    echo ""
+    functor=$1
+    args=(${@:2})
+    arg_path=$( IFS=$'/'; echo "${args[*]}" )
+    in_tree "root/$functor/$arg_path"
+    if [ "$return" =  "" ]; then
+	echo "false"
+    else
+	echo "true"
+    fi
 }
 
 function r {
-    echo ""
+    echo ${@}
 }
 
 
-
+f father m n
 f father a b
-f father b c
-f father n m
-f father n i
-f father n v
-f father m o
-f father o p
-f father b c
-f father b z
-f cats a b
-f cats b l
+f father a b c
+# f father b c
+q father a b
+q father b c
+r grandfather X Y :- father X Z :and father Z Y
+
+# f father n m
+# f father n i
+# f father n v
+# f father m o
+# f father o p
+# f father b c
+# f father b z
+# f cats a b
+#f cats b l
 # I think those break
-# f father z a
-# f father y c 
+# f father c b
+
+#f father y c 
 
 tree root
 rm -r root
