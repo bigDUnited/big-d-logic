955afb7af812f183038be923bde5ca9c20decf67
Boyko Surlev / Бойко Сурлев <cph-bs96@cphbusiness.dk>
2016-11-14 16:38:08 +0100

    Implement: Fully working query and parents array
    Fixed some issues with querying and added parents array to every node obj

--- KotlinAlternative.kt
+++ KotlinAlternative.kt
@@ -1,11 +1,9 @@
 import java.util.*
 
-//? = allow nulls
-
-class Node constructor(parentNode: Node?, nodeName: String, childenNode: ArrayList<Node>?) {
-    private var parent : Node? = parentNode;
-    private var name : String = nodeName
-    private var children : ArrayList<Node>? = childenNode
+class Node constructor(parentsNode: ArrayList<Node>?, nodeName: String, childrenNode: ArrayList<Node>?) {
+    private var parents : ArrayList<Node>? = parentsNode;
+    private var name : String = nodeName;
+    private var children : ArrayList<Node>? = childrenNode;
 
     fun getName(): String {
        return name.toString();
@@ -19,8 +17,30 @@ class Node constructor(parentNode: Node?, nodeName: String, childenNode: ArrayLi
         return children!!;
     }
 
-    fun getParent(): Node {
-        return parent!!;
+    fun addParent(element: Node?): Boolean {
+        return parents!!.add( element!! );
+    }
+
+    fun getParents(): ArrayList<Node> {
+        return parents!!;
+    }
+
+    override fun toString(): String {
+        var parentsString = "Parents : [-";
+
+        for (parent in parents!!) {
+            parentsString += parent.getName() + "-";
+        }
+        parentsString += "]";
+
+        var childrenString = "Children : [-";
+
+        for (child in children!!) {
+            childrenString += child.getName() + "-";
+        }
+        childrenString += "]";
+
+        return parentsString + " [*" + this.getName() + "*] " + childrenString;
     }
 }
 
@@ -36,7 +56,7 @@ class Query( masterNode: Node ) {
     }
 
     private fun fatherSearch( parentChild: Node, childName: String ): Node {
-        var child : Node = Node(null, "", null);
+        var child : Node = Node(ArrayList<Node>(), "", ArrayList<Node>());
         if (parentChild.getName() == childName) {
             child = parentChild;
             println("[FATHER] Correct child: " + parentChild.getName());
@@ -49,7 +69,7 @@ class Query( masterNode: Node ) {
     }
 
     private fun grandfatherSearch( parentChild: Node, childName: String ): Node {
-        var child : Node = Node(null, "", null);
+        var child : Node = Node(ArrayList<Node>(), "", ArrayList<Node>());
         for (grandchildElem in parentChild.getChildren()) {
             if (grandchildElem.getName() == childName) {
                 child = grandchildElem;
@@ -63,8 +83,8 @@ class Query( masterNode: Node ) {
     }
 
     private fun find( currentRoot: Node, fatherName: String, childName: String, state : String ): Boolean {
-        var parent : Node = Node(null, "", null);
-        var child : Node = Node(null, "", null);
+        var parent : Node = Node(ArrayList<Node>(), "", ArrayList<Node>());
+        var child : Node = Node(ArrayList<Node>(), "", ArrayList<Node>());
 
         var safeState = "father";
         if(state == "grandfather") { safeState = state; }
@@ -78,7 +98,7 @@ class Query( masterNode: Node ) {
             println("[FIND] Traverse from root: " + currentRoot.getName() + " to its children");
 
             for (godElem in currentRoot.getChildren()) {
-                println("[FIND] Plausable root: " + godElem.getName() + ", Child: " + childName );
+                println("[FIND] Plausible root: " + godElem.getName() + ", Child: " + childName );
 
                 var result = find(godElem, fatherName, childName, state);
                 if (result == true ) return result;
@@ -110,44 +130,82 @@ class Query( masterNode: Node ) {
 
 fun traceFather( masterNode: Node, candidateFatherNode: Node ): Node? {
     for (masterChildElem in masterNode.getChildren()) {
-        if ( masterChildElem.getName() == candidateFatherNode.getName() ) {  return masterChildElem; }
+        if ( masterChildElem.getName() == candidateFatherNode.getName() ) {
+            //println("Before 1" + masterChildElem.getName());
+            return masterChildElem;
+        }
 
         var tracingResult = traceFather(masterChildElem, candidateFatherNode);
-        if ( tracingResult != null ) { return tracingResult; }
+        if ( tracingResult != null ) {
+            //println("Before 2" + masterChildElem.getName());
+            return tracingResult;
+        }
     }
     return null
 }
 
 fun fact( masterNode: Node, fatherName: String, childName: String ) {
-    var candidateFatherNode = Node( null, fatherName, ArrayList<Node>() );
-    var candidateChildNode = Node( null, childName, ArrayList<Node>() );
+    println("fatherName: " + fatherName + ", childName: " + childName);
+    var candidateFatherNode = Node( ArrayList<Node>(), fatherName, ArrayList<Node>() );
+    var candidateChildNode = Node( ArrayList<Node>(), childName, ArrayList<Node>() );
 
     var father = traceFather(masterNode, candidateFatherNode);
     var child = traceFather(masterNode, candidateChildNode);
 
     if ( child == null ) {
         if ( father != null ) {
+            println("1");
+            candidateChildNode.addParent(father);
             father.addChild(candidateChildNode);
         } else if (fatherName == masterNode.getName()) {
+            println("2");
+            candidateChildNode.addParent(masterNode);
             masterNode.addChild(candidateChildNode);
         } else {
+            println("3");
+            candidateChildNode.addParent(candidateFatherNode);
             candidateFatherNode.addChild( candidateChildNode );
+            candidateFatherNode.addParent(masterNode);
             masterNode.addChild( candidateFatherNode )
         }
     } else {
-        if ( father != null ) {
-            father.addChild(child);
+        if ( father == null ) {
+            if ( candidateFatherNode.getName() == masterNode.getName() ) {
+                println("5.1 " + candidateChildNode.getName());
+                masterNode.addChild(child);
+                child.addParent(masterNode);
+            } else {
+                println("5.2 " + candidateChildNode.getName());
+                candidateFatherNode.addParent(masterNode);
+                child.addParent(candidateFatherNode);
+                candidateFatherNode.addChild(child);
+                masterNode.addChild(candidateFatherNode);
+            }
         } else {
-            masterNode.addChild(candidateFatherNode);
-            candidateFatherNode.addChild(candidateChildNode);
+            println("4 " + child.getName());
+            father.addChild(child);
+            child.addParent(father);
         }
     }
 }
 
+//fun rule( masterNode: Node, fatherName: String, childName: String ){
+//    var candidateFatherNode = Node( ArrayList<Node>(), fatherName, ArrayList<Node>() );
+//    var candidateChildNode = Node( ArrayList<Node>(), childName, ArrayList<Node>() );
+//
+//    var father = traceFather(masterNode, candidateFatherNode);
+//    var child = traceFather(masterNode, candidateChildNode);
+//}
+
 fun exposeStructure( masterNode : Node, preventOverflow: ArrayList<Node>)  {
 
+    if( masterNode.getParents().size == 0 ) {
+        println( "[EXPOSESTRUCTURE] *ActualM* :" + masterNode.toString());
+    }
+
     for (child in masterNode.getChildren()){
-        println( "[EXPOSESTRUCTURE] Father: '" + masterNode.getName() + "', Child: '" + child.getName() + "'" );
+        //println( "[EXPOSESTRUCTURE] Father: '" + masterNode.getName() + "', Child: '" + child.getName() + "'" );
+        println( "[EXPOSESTRUCTURE] *Actual* :" + child.toString());
 
         var isExisting = false;
         for (goner in preventOverflow){
@@ -167,33 +225,50 @@ fun exposeStructure( masterNode : Node, preventOverflow: ArrayList<Node>)  {
 }
 
 fun main(args: Array<String>) {
-    var masterNode = Node(null, "A", ArrayList<Node>() );
+    var masterNode = Node(ArrayList<Node>(), "A", ArrayList<Node>() );
 
     //Normal rotation
     fact(masterNode, "A", "B");
-    fact(masterNode, "B", "BB");
-    fact(masterNode, "B", "E");
-    fact(masterNode, "E", "F");
-    fact(masterNode, "E", "G");
-    
-    fact(masterNode, "A", "C");
-    fact(masterNode, "C", "D");
-    fact(masterNode, "D", "W");
+    fact(masterNode, "B", "C");
+    fact(masterNode, "B", "D");
+    fact(masterNode, "D", "E");
 
-    //Mixup connections
-    fact(masterNode, "E", "A");
-    fact(masterNode, "Q", "D");
+    fact(masterNode, "QQQ", "B");
 
-    fact(masterNode, "E", "123");
+    fact(masterNode, "A", "F");
+    fact(masterNode, "G", "H");
+    fact(masterNode, "A", "D");
+
+    fact(masterNode, "F", "E");
+
+
+//    fact(masterNode, "E", "F");
+//    fact(masterNode, "E", "G");
+//
+//    fact(masterNode, "A", "H");
+//    fact(masterNode, "H", "I");
+//    fact(masterNode, "H", "J");
+//
+//    fact(masterNode, "Z", "ZZ");
+//    fact(masterNode, "C", "D");
+//    fact(masterNode, "D", "W");
+//
+//    //Mixup connections
+//    fact(masterNode, "E", "A");
+//    fact(masterNode, "Q", "D");
+//
+//    fact(masterNode, "E", "123");
 
     println("-------------------");
     var preventOverflow = ArrayList<Node>();
     preventOverflow.add( masterNode );
     exposeStructure(masterNode, preventOverflow );
 
-    var queryObj = Query(masterNode);
-    println("-------------------");
-    println("[MAIN] Result is: " + queryObj.father("E", "123"));
+//    var queryObj = Query(masterNode);
+//    println("-------------------");
+//    println("[MAIN] Result is: " + queryObj.father("E", "123"));
+//
+//    println( masterNode.toString() );
 //    println("-------------------");
 //    println("[MAIN] Result is: " + queryObj.father("A", "QPDASDASDAS") );
 //    println("-------------------");
